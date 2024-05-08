6d15bab61e678b655e14151fd0f7b6bdee89a415
Hendrik Mennen <hmennen@protop-solutions.com>
2024-05-08 14:00:12 +0200

    update

--- src/OneWare.Vhdp/Compiler/VhdpPreCompileStep.cs
+++ src/OneWare.Vhdp/Compiler/VhdpPreCompileStep.cs
@@ -8,9 +8,9 @@ public class VhdpPreCompileStep(ILogger logger) : IFpgaPreCompileStep
 {
     public string Name => "VHDP Compiler";
 
-    public Task PerformPreCompileStepAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    public Task<bool> PerformPreCompileStepAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
     {
         logger.Warning("VHDP Compiler is not implemented yet!", null, true, true);
-        return Task.CompletedTask;
+        return Task.FromResult(false);
     }
 }
